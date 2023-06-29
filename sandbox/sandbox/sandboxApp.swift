d65ff955d8af09ab6c8d6ba165c5527d98388e7e
sinano1107 <tftf161107@icloud.com>
2023-06-29 11:19:38 +0900

    Add xcode project

---
+++ sandbox/sandbox/sandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  sandboxApp.swift
+//  sandbox
+//
+//  Created by 長政輝 on 2023/06/29.
+//
+
+import SwiftUI
+
+@main
+struct sandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
